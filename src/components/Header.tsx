7c9ec78a41410d72aee8a695f5bbce575f63abd5
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2021-03-24 22:13:11 +0900

    Headerにアイコンを並べるようにした

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,35 +4,13 @@ import { transparentize } from 'polished'
 import { Link } from 'gatsby'
 import { Button, Typography, IconButton, Toolbar, AppBar } from '@material-ui/core'
 import MenuIcon from '@material-ui/icons/Menu'
+import AccountCircle from '@material-ui/icons/AccountCircle';
 import { makeStyles } from '@material-ui/core/styles'
+import MailIcon from '@material-ui/icons/Mail';
+import NotificationsIcon from '@material-ui/icons/Notifications';
 
 import { heights, dimensions, colors } from '../styles/variables'
 import Container from './Container'
-
-const StyledHeader = styled.header`
-  height: ${heights.header}px;
-  padding: 0 ${dimensions.containerPadding}rem;
-  background-color: ${colors.brand};
-  color: ${transparentize(0.5, colors.white)};
-`
-
-const HeaderInner = styled(Container)`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  height: 100%;
-`
-
-const HomepageLink = styled(Link)`
-  color: ${colors.white};
-  font-size: 1.5rem;
-  font-weight: 600;
-
-  &:hover,
-  &:focus {
-    text-decoration: none;
-  }
-`
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -43,7 +21,13 @@ const useStyles = makeStyles(theme => ({
   },
   title: {
     flexGrow: 1
-  }
+  },
+  sectionDesktop: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
+    },
+  },
 }))
 
 interface HeaderProps {
@@ -52,13 +36,36 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ title }) => {
   const classes = useStyles()
+  const handleProfileMenuOpen = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+  };
+
   return (
     <AppBar position="static">
       <Toolbar>
         <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
           <MenuIcon />
         </IconButton>
-        <Button color="inherit">Login</Button>
+        <Typography variant="h6" className={classes.title}>
+          {title}
+        </Typography>
+        <div className={classes.sectionDesktop}>
+          <IconButton aria-label="show 4 new mails" color="inherit">
+            <MailIcon />
+          </IconButton>
+          <IconButton aria-label="show 17 new notifications" color="inherit">
+            <NotificationsIcon />
+          </IconButton>
+          <IconButton
+            edge="end"
+            aria-label="account of current user"
+            aria-controls="primary-search-account-menu"
+            aria-haspopup="true"
+            onClick={handleProfileMenuOpen}
+            color="inherit"
+          >
+            <AccountCircle />
+          </IconButton>
+        </div>
       </Toolbar>
     </AppBar>
   )
