3e2888c1c024bdd9a65131057093afa739104e7d
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2021-03-27 04:28:56 +0900

    GLTFExportしてみた

--- src/threeds/three-scene.tsx
+++ src/threeds/three-scene.tsx
@@ -1,17 +1,20 @@
-import * as React from 'react'
 import { WebGLRenderer, Scene, PerspectiveCamera, DirectionalLight, Color } from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { GLTFExporter } from "three/examples/js/exporters/GLTFExporter";
 import { VRM } from '@pixiv/three-vrm'
 export class ThreeScene {
   private canvas: HTMLCanvasElement
-  private scene: Scene | null = null
+  private scene: Scene
+  private renderer: WebGLRenderer
+  private objScene: Scene | null = null
   private camera: PerspectiveCamera | null = null
-  private renderer: WebGLRenderer | null = null
   private frameId: number | null = null
 
   constructor(canvas: HTMLCanvasElement) {
     this.canvas = canvas
+    this.scene = new Scene()
+    this.renderer = new WebGLRenderer({ canvas: this.canvas, antialias: true })
     this.animate = this.animate.bind(this)
     this.initScene();
   }
@@ -22,9 +25,9 @@ export class ThreeScene {
       url,
       (gltf: Scene) => {
         VRM.from(gltf).then((vrm: VRM) => {
-          if (this.scene) {
-            this.scene.add(vrm.scene)
-          }
+          console.log(vrm)
+          this.scene.add(vrm.scene)
+          this.objScene = vrm.scene
         })
       },
       (progress: ProgressEvent) => {
@@ -37,30 +40,44 @@ export class ThreeScene {
   }
 
   exportFile(fileType: string){
+    if(!this.objScene){
+      return;
+    }
+    const exporter = new GLTFExporter();
+    exporter.parse(this.objScene, (gltf: any) => {
+      console.log( gltf );
+      const blob =  new Blob([JSON.stringify(gltf)], { type: 'text/json' });
+      this.downloadFile("sample.gltf", blob)
+    }, {});
+  }
 
+  downloadFile(fileName: string, fileBlob: Blob){
+    const jsonURL = window.URL.createObjectURL(fileBlob);
+    const link = document.createElement('a');
+    document.body.appendChild(link);
+    link.href = jsonURL;
+    link.setAttribute('download', fileName);
+    link.click();
+    document.body.removeChild(link);
   }
 
   private initScene() {
-    const renderer = new WebGLRenderer({ canvas: this.canvas, antialias: true })
     const width = this.canvas.clientWidth
     const height = this.canvas.clientHeight
-    const scene = new Scene()
-    scene.background = new Color(0x212121)
+    this.scene.background = new Color(0x212121)
 
     const directionalLight = new DirectionalLight(0xffffff)
     directionalLight.position.set(0, 1, -2)
-    scene.add(directionalLight)
+    this.scene.add(directionalLight)
 
-    this.scene = scene
     const camera = new PerspectiveCamera(50, width / height, 0.01)
     camera.position.set(0, 1.5, -1.5)
-    const controls = new OrbitControls(camera, renderer.domElement)
+    const controls = new OrbitControls(camera, this.renderer.domElement)
     controls.target.set(0, 0.75 * 1.5, 0)
     controls.update()
     this.camera = camera
-    renderer.setSize(width, height)
-    renderer.setPixelRatio(window.devicePixelRatio)
-    this.renderer = renderer
+    this.renderer.setSize(width, height)
+    this.renderer.setPixelRatio(window.devicePixelRatio)
     this.animate()
   }
 
@@ -77,17 +94,5 @@ export class ThreeScene {
     if (this.renderer && this.scene && this.camera) {
       this.renderer.render(this.scene, this.camera)
     }
-  }
-
-  render() {
-    const loadVrmUrl = this.props.url
-    if (loadVrmUrl) {
-      this.loadVRM(loadVrmUrl)
-    }
-    return (
-      <div>
-        <canvas style={{ width: '80vw', height: '40vw' }} ref={this.onCanvasLoaded} />
-      </div>
-    )
   }
 }
